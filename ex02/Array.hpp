ec66388bc83ef13e56b44014228fe2298e85101e
Marc <Nope>
2023-09-22 16:39:27 +0200

    empty array

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 12:15:18 by mjourno           #+#    #+#             */
-/*   Updated: 2023/09/22 16:38:06 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/09/22 16:39:08 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class	Array {
 		T		*_array;
 		size_t	_size;
 	public:
-		Array() : _array(NULL), _size(0) {};
+		Array() : _array(new T[0]), _size(0) {};
 		Array(unsigned int n) : _array(new T[n]), _size(n) { for(unsigned int i = 0; i < n; i++) _array[i] = T(); };
 		Array(const Array<T> &src) : _array(new T[src.size()]), _size(src.size()) { *this = src; };
 
