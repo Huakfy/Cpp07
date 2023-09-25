c7d67f143b1cf1404fb1dd758bf643d803480245
Marc JOURNO <mjourno@made-f0Br7s6.clusters.42paris.fr>
2023-09-25 13:30:10 +0200

    fucking const

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:00:39 by mjourno           #+#    #+#             */
-/*   Updated: 2023/09/22 12:10:59 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/09/25 13:29:04 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <string>
 
 template<typename T>
-void	iter(T *addr, size_t size, void (*f)(T&)) {
+void	iter(T *addr, size_t size, void (*f)(T const &)) {
 	for (size_t i = 0; i < size; i++)
 		f(addr[i]);
 }
